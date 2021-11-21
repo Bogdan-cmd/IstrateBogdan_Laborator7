9fd3b46ee837b7fdd6343e48fa6a7169d4a74e60
Bogdan-cmd <bogdan.d.istrate@gmail.com>
2021-11-21 21:32:55 +0200

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IstrateBogdan_Laborator2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
