6d00faf11375061e882915132f898ad8894526bc
Bogdan-cmd <bogdan.d.istrate@gmail.com>
2022-01-02 19:22:39 +0200

    -lab11-

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcCustomersService;
+using LibraryModel.Models;
+
+namespace IstrateBogdan_Laborator2.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
